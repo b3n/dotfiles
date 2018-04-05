672aa853734e27f7e7c3cefc2e30edce5974bc4d
Ben <ben@shobute.com>
2018-04-05 17:55:28 +0100

    Make ERC quieter.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -44,10 +44,21 @@
 
 (show-paren-mode 1)
 
+
 (setq erc-lurker-hide-list '("JOIN" "PART" "QUIT")
       erc-lurker-threshold-time 3600)
 (setq erc-join-buffer 'bury)
 (setq erc-nick "Ben`")
+(setq erc-format-query-as-channel-p t
+      erc-track-priority-faces-only 'all
+      erc-track-faces-priority-list '(erc-error-face
+				      erc-current-nick-face
+				      erc-keyword-face
+				      erc-nick-msg-face
+				      erc-direct-msg-face
+				      erc-dangerous-host-face
+				      erc-notice-face
+				      erc-prompt-face))
 
 
 ;;; Some ansi-term advice, stolen from http://echosa.github.io/blog/2012/06/06/improving-ansi-term/.
