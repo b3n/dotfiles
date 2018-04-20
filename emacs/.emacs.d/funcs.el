e6435d767dd5c7abdab3034ff0bd4447bd4f6e6e
Ben Lloyd <ben.lloyd@skyscanner.net>
2018-04-20 15:01:30 +0100

    Emacs: Moved use-package init to its own function.
    To help keep the main config a little cleaner, as this function
    should never change.

--- emacs/.emacs.d/funcs.el
+++ emacs/.emacs.d/funcs.el
@@ -1,6 +1,7 @@
 ;; http://camdez.com/blog/2013/11/14/emacs-show-buffer-file-name/
 (defun my-show-buffer-file-name ()
   "Show the full path to the current file in the minibuffer."
+
   (interactive)
   (let ((file-name (buffer-file-name)))
     (if file-name
@@ -8,3 +9,24 @@
           (message file-name)
           (kill-new file-name))
       (error "Buffer not visiting a file"))))
+
+
+(defun my-use-package-initialize ()
+  "Install/configure use-package and dependencies."
+
+  (package-initialize)
+
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+  (package-initialize)
+
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+  (eval-when-compile (require 'use-package))
+  (setq use-package-always-ensure t)
+
+  (use-package use-package-ensure-system-package :config (exec-path-from-shell-initialize))
+  (use-package diminish)
+  (use-package general))
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,30 +2,25 @@
 (unless (server-running-p)
   (server-start))
 
+
 (setq custom-file (make-temp-file "emacs-custom")
       create-lockfiles nil
       make-backup-files nil)
-
-(package-initialize)
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile (require 'use-package))
-(setq use-package-always-ensure t)
-
-(use-package use-package-ensure-system-package :config (exec-path-from-shell-initialize))
-(use-package diminish)
-(use-package general)
-
 (setq my-prefix "<f5>")
+(setq browse-url-generic-program "firefox")
+(setq-default tab-width 4
+	      indent-tabs-mode nil)
+
 
 (load "~/.emacs.d/funcs.el")
+
+(my-use-package-initialize)
+
+(mapc 'load (file-expand-wildcards "~/.emacs.d/packages/*.el"))
+
+(cond ((eq window-system 'ns) (load "~/.emacs.d/platform/osx.el"))
+      ((eq window-system 'x) (load "~/.emacs.d/platform/nix.el")))
+
 
 (general-define-key :prefix my-prefix
 		    "f s" #'save-buffer
@@ -34,9 +29,6 @@
 
 (general-define-key :keymaps 'minibuffer-inactive-mode-map [mouse-1] nil) 
 
-(setq browse-url-generic-program "firefox")
-(setq-default tab-width 4
-	      indent-tabs-mode nil)
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -46,8 +38,3 @@
 (winner-mode 1)
 (recentf-mode 1)
 (flyspell-mode)
-
-(mapc 'load (file-expand-wildcards "~/.emacs.d/packages/*.el"))
-
-(cond ((eq window-system 'ns) (load "~/.emacs.d/platform/osx.el"))
-      ((eq window-system 'x) (load "~/.emacs.d/platform/nix.el")))
