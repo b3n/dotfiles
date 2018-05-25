0dba7d6f705bd4b743e6bddcfa41d8376f9a3b3d
Ben Lloyd <ben.lloyd@skyscanner.net>
2018-05-25 16:25:03 +0100

    Emacs: Set consistent winum bindings.

--- emacs/.emacs.d/packages/winum.el
+++ emacs/.emacs.d/packages/winum.el
@@ -1,13 +1,13 @@
 (use-package winum
   :general (my-leader-def
-             "1" #'winum-select-window-1
-             "2" #'winum-select-window-2
-             "3" #'winum-select-window-3
-             "4" #'winum-select-window-4
-             "5" #'winum-select-window-5
-             "6" #'winum-select-window-6
-             "7" #'winum-select-window-7
-             "8" #'winum-select-window-8
-             "9" #'winum-select-window-9)
+             "m" #'winum-select-window-1
+             "," #'winum-select-window-2
+             "." #'winum-select-window-3
+             "n" #'winum-select-window-4
+             "e" #'winum-select-window-5
+             "i" #'winum-select-window-6
+             "l" #'winum-select-window-7
+             "u" #'winum-select-window-8
+             "y" #'winum-select-window-9)
   :custom (winum-auto-setup-mode-line nil)
   :config (winum-mode))
