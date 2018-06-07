6ad1830d5f95dd4710732958629a4a20001b9593
Ben Lloyd <ben.lloyd@skyscanner.net>
2018-06-07 10:35:38 +0100

    Added numeric key bindings to winum.

--- emacs/.emacs.d/packages/winum.el
+++ emacs/.emacs.d/packages/winum.el
@@ -1,5 +1,14 @@
 (use-package winum
   :general (my-leader-def
+             "1" #'winum-select-window-1
+             "2" #'winum-select-window-2
+             "3" #'winum-select-window-3
+             "4" #'winum-select-window-4
+             "5" #'winum-select-window-5
+             "6" #'winum-select-window-6
+             "7" #'winum-select-window-7
+             "8" #'winum-select-window-8
+             "9" #'winum-select-window-9
              "m" #'winum-select-window-1
              "," #'winum-select-window-2
              "." #'winum-select-window-3
