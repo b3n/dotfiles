276f00dadc8be6410a8b8a84977a712cf9c0becf
Ben Lloyd <ben.lloyd@skyscanner.net>
2018-08-16 11:21:12 +0100

    Emacs: Remove background colour from search matches.
    Previously it was impossible to see what region you were selecting
    within a search match, this removes the background (replacing it with
    bold italics) so the region selection can be seen.

--- emacs/.emacs.d/packages/theme.el
+++ emacs/.emacs.d/packages/theme.el
@@ -2,4 +2,6 @@
   :init
   (setq org-fontify-whole-heading-line t)
   :config
-  (load-theme 'leuven t))
+  (load-theme 'leuven t)
+  (custom-set-faces
+   `(lazy-highlight ((t (:background nil :weight bold :slant italic)))))
