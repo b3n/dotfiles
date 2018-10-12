034ced60d6658cd6f4e83a5c2bb1e92b7732da72
Ben Lloyd <ben.lloyd@skyscanner.net>
2018-10-12 17:21:12 +0100

    Added binding for `avy-goto-line`.

--- emacs/.emacs.d/packages/avy.el
+++ emacs/.emacs.d/packages/avy.el
@@ -1,5 +1,7 @@
 (use-package avy
-  :general (my-leader-def "a" #'avy-goto-char)
+  :general (my-leader-def
+             "a" #'avy-goto-char
+             "A" #'avy-goto-line)
   :custom
   (avy-keys '(?a ?r ?s ?t ?d ?h ?n ?e ?i ?o))
   :config
