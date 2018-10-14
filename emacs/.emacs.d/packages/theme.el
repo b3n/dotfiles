f0cf66b2ec05496d3490e8605d6df0e8874ce749
Ben <58246+b3n@users.noreply.github.com>
2018-10-14 21:14:51 +0100

    Trying out spacemacs-light theme.

--- emacs/.emacs.d/packages/theme.el
+++ emacs/.emacs.d/packages/theme.el
@@ -1,5 +1,10 @@
 (use-package leuven-theme
+  :disabled
   :init
   (setq org-fontify-whole-heading-line t)
   :config
   (load-theme 'leuven t))
+
+(use-package spacemacs-common
+  :ensure spacemacs-theme
+  :config (load-theme 'spacemacs-light t))
