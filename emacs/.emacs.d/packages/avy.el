8175caa92c5499605724bccf972292f000c896a2
Ben Lloyd <blloyd@OXF-LGMXG5SBJG.local>
2018-12-10 16:29:29 +0000

    Emacs: Removed unused packages.
    Bye bye avy and coffee.

--- emacs/.emacs.d/packages/avy.el
+++
@@ -1,8 +0,0 @@
-(use-package avy
-  :general (my-leader-def
-             "a" #'avy-goto-char
-             "A" #'avy-goto-line)
-  :custom
-  (avy-keys '(?a ?r ?s ?t ?d ?h ?n ?e ?i ?o))
-  :config
-  (avy-setup-default))
