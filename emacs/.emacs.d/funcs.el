8b74cf274827390b0f88fc51fff50a75252d529c
Ben <58246+b3n@users.noreply.github.com>
2019-03-03 21:35:00 +0000

    Emacs: Switched to stable MELPA.

--- emacs/.emacs.d/funcs.el
+++ emacs/.emacs.d/funcs.el
@@ -15,7 +15,7 @@
   "Install/configure use-package and dependencies."
 
   (require 'package)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("melpa" . "https://stable.melpa.org/packages/") t)
   (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
   (package-initialize)
 
