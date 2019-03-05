5555545822df6702ad5494b4c260d4cc238e130a
Ben <58246+b3n@users.noreply.github.com>
2019-03-05 10:50:02 +0000

    Set package archive priorities.

--- emacs/.emacs.d/funcs.el
+++ emacs/.emacs.d/funcs.el
@@ -17,8 +17,16 @@
   "Install/configure use-package and dependencies."
 
   (require 'package)
-  (add-to-list 'package-archives '("melpa" . "https://stable.melpa.org/packages/") t)
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+  (setq package-archives
+        '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
+          ("MELPA Stable" . "https://stable.melpa.org/packages/")
+          ("MELPA"        . "https://melpa.org/packages/")
+          ("ORG"          . "https://orgmode.org/elpa/"))
+        package-archive-priorities
+        '(("MELPA Stable" . 10)
+          ("ORG"          . 6)
+          ("GNU ELPA"     . 5)
+          ("MELPA"        . 0)))
   (package-initialize)
 
   (unless (package-installed-p 'use-package)
