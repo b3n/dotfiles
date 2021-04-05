9c3cff9af0c0c0dcd434d044e843b0f3f7f92f17
ben <ben@puffy.lan>
2021-04-06 08:47:04 +1000

    Evil tweaks

--- emacs/.config/emacs/lisp/init-evil.el
+++ emacs/.config/emacs/lisp/init-evil.el
@@ -6,12 +6,12 @@
   (setq evil-want-keybinding nil)
 
   :bind (:map evil-insert-state-map
-         ;; This is needed because we disable evil insert state bindings, but still want C-w.
          ("C-w" . evil-window-map)
 
          :map evil-motion-state-map
-         ("RET"     . push-button)
-         ("SPC"     . evil-execute-in-emacs-state)
+         ("RET")
+         ("SPC" . evil-execute-in-emacs-state)
+         ("i"   . evil-insert)
 
          :map evil-normal-state-map
          ("U"   . undo)
@@ -19,11 +19,12 @@
          ("q"   . bury-buffer)
 
          :map evil-visual-state-map
-         ("U"   . undo)
+         ("U" . undo)
          ("v" . evil-visual-line)
 
          :map evil-window-map
-         ("C-f" . other-frame))
+         ("C-f" . other-frame)
+         ("f"   . other-frame))
 
   :custom
   (evil-disable-insert-state-bindings t)
@@ -36,19 +37,31 @@
   (evil-want-minibuffer t)
 
   :config
-  (setq evil-insert-state-modes (append evil-insert-state-modes evil-emacs-state-modes))
+  (setq evil-insert-state-modes (append evil-insert-state-modes evil-emacs-state-modes '(exwm-mode)))
   (setq evil-emacs-state-modes nil)
 
   (evil-mode 1))
 
 
 (use-package evil-surround
-  ;; TODO: Setup better bindings (e.g. use `s` instead of `ys`)
   :after evil
   :straight t
 
+  :bind (:map evil-normal-state-map
+         ("s" . evil-surround-edit)
+         ("S" . evil-Surround-edit)
+         :map evil-operator-state-map
+         ("s" . evil-surround-edit)
+         ("S" . evil-Surround-edit)
+         :map evil-visual-state-map
+         ("s" . evil-surround-region)
+         ("S" . evil-Surround-region))
+
   :config
-  (global-evil-surround-mode 1))
+  ;; Hotfix for repeating surrounds (upstream assumes "ys" binding).
+  (defun evil-surround-call-with-repeat (callback)
+    (let ((evil-surround-record-repeat t))
+      (call-interactively callback))))
 
 
 (use-package winner
@@ -56,9 +69,9 @@
   :demand
 
   :bind (:map evil-window-map
-    ("u"   . winner-undo)
-    ("C-u" . winner-undo)
-    ("C-r" . winner-redo))
+         ("u"   . winner-undo)
+         ("C-u" . winner-undo)
+         ("C-r" . winner-redo))
 
   :config
   (winner-mode 1))
