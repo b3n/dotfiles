8d39d164baad2801dbf72a29cd6e7021dbef2f1d
ben <ben@puffy.lan>
2021-09-17 08:58:57 +1000

    Add evil-exchange

--- emacs/.config/emacs/lisp/init-evil.el
+++ emacs/.config/emacs/lisp/init-evil.el
@@ -5,6 +5,30 @@
   :init
   (setq evil-want-keybinding nil)
 
+  ;; TODO: Remove this after upgrading emacs to a version which includes undo-redo.
+  (defun undo--last-change-was-undo-p (undo-list)
+    (while (and (consp undo-list) (eq (car undo-list) nil))
+      (setq undo-list (cdr undo-list)))
+    (gethash undo-list undo-equiv-table))
+  (defun undo-redo (&optional arg)
+    "Undo the last ARG undos."
+    (interactive "*p")
+    (cond
+     ((not (undo--last-change-was-undo-p buffer-undo-list))
+      (user-error "No undo to undo"))
+     (t
+      (let* ((ul buffer-undo-list)
+             (new-ul
+              (let ((undo-in-progress t))
+                (while (and (consp ul) (eq (car ul) nil))
+                  (setq ul (cdr ul)))
+                (primitive-undo arg ul)))
+             (new-pul (undo--last-change-was-undo-p new-ul)))
+        (message "Redo%s" (if undo-in-region " in region" ""))
+        (setq this-command 'undo)
+        (setq pending-undo-list new-pul)
+        (setq buffer-undo-list new-ul)))))
+
   :bind (:map evil-insert-state-map
          ("C-w" . evil-window-map)
 
@@ -62,6 +86,13 @@
   (defun evil-surround-call-with-repeat (callback)
     (let ((evil-surround-record-repeat t))
       (call-interactively callback))))
+
+
+(use-package evil-exchange
+  :after evil
+  :straight t
+  :config
+  (evil-exchange-install))
 
 
 (use-package winner
